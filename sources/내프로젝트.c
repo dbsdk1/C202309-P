dd1fc809a8c35fb9a164ad96cdf13c1286bd0969
dbsdk1 <spkwnf081620@naver.com>
2023-11-26 02:56:59 +0900

    프로젝트 업로드

--- sources/내프로젝트.c
+++ sources/내프로젝트.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define yeol 50
 #define bunho 20
 
@@ -14,7 +16,7 @@ int main() {
         "1. 피부타입 진단하기\n2. 나의 화장품 서랍\n3. 오늘의 피부 일기\n4. "
         "올리브영 바로가기\n5. 여드름 관련 정보 모음zip\n");
     printf("6. 프로그램 종료\n");
-    /*printf("오늘(날짜~)은 약 먹는 날이 %s\n", med, (int)sizeof(med));*/
+    /*printf("오늘(날짜~)은 약 먹는 날이 %s\n", med;*/
     printf("--------------------------\n");
     scanf_s("%d", &choice);
 
@@ -23,21 +25,28 @@ int main() {
       printf("피부타입 진단을 시작할게요 :)\n");
       printf("피부타입은 크게 지성, 복합성(중성), 건성으로 나누어져요!\n");
       printf("다음 보기들을 보고 자신의 피부에 맞는 것을 고르면 됩니다.\n");
-      printf("");
-      scanf_s("%d", &type1);
-
+      printf("세수하고 나오면 얼굴이 건조하고 ");
+      scanf_s("%d", &type1);  // 이걸 뭐로구현해야지..? 동적메모리? 한 5개 정도 질문지 만들고 1번이..? sum으로 해야하나 아하 그래야할듯;.? 함수만들어버리기?
+      //배열 칸에 1, 2 각각 저장 후 for문으로 
     } else if (choice == 2) {
       int num = 0;
-      char save[bunho][yeol] = {" "};
+     
+      char save[bunho][yeol] = {""};  // 동적할당 ㄱㄱㄱㄱ
       // 화장품 저장소 2차원배열 만들어서 그 안에 화장품 이름 저장하기, 목록은
       // 바로 출력하기
-      /* for (int i = 0; i < bunho; i++) {
-        printf("%d. %s\n", i+1, save[i], (int)sizeof(save));  여기 경고뜸!!
-      그리고 저장안되면(배열 빈칸이면) 출력안되게 해야한다.
-      } */
+      for (int i = 0; i < bunho; i++) {
+        if (strlen(save[i]) >= 1) {
+          printf("%d. %s\n", i + 1, save[i]);
+        }
+        
+      //그리고 저장안되면(배열 빈칸이면) 출력안되게 해야한다. -> 문자열길이함수사용!
+      } 
 
       printf("1. 화장품 추가\n2. 나가기\n");
       scanf_s("%d", &num);
+      if (num == 1) {
+        printf("");
+      }
     } else if (choice == 3) {
       int num = 0;
       printf(
@@ -56,7 +65,9 @@ int main() {
       } else if (num == 2) {
         // printf("날짜수정할건지 일기내용을수정할건지 고르라고 하기");
       } else if (num == 3) {
-      } else {
+        printf("삭제할 일기의 번호를 입력하세요.");
+      } else if(num==4) {
+          // 배열 for문으로 불러오기
       }
     } else if (choice == 4) {
       printf(
