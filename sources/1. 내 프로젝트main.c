87968793a7d79c7bbae4dc686ae6d10c2b453c53
dbsdk1 <spkwnf081620@naver.com>
2023-12-06 23:42:13 +0900

    파일입출력 사용 업데이트

--- sources/1. 내 프로젝트main.c
+++ sources/1. 내 프로젝트main.c
@@ -3,10 +3,13 @@
 #include <string.h>
 #define yeol 100
 #define bunho 20
+#define MAX 10000
 
-char save[bunho][yeol] = {""};
 int type[5];
 int count = 0;
+char line[MAX];
+char content[MAX] = "";
+int check = 1;
 
 struct HwaInfo {
   char* brandname;
@@ -17,6 +20,17 @@ struct HwaInfo {
 
 void addHwaInfo(struct HwaInfo* hwa);
 void displayHwaInfo(const struct HwaInfo* hwa, int count);
+
+int errordetect(int user_num, int limit_num) {
+  if (user_num > limit_num || user_num <= 0) {
+    check = 1;
+    printf("잘못된 입력입니다. 1~%d 사이의 숫자를 입력해주세요!\n", limit_num);
+    // 조건파악
+  } else {
+    check = 0;
+  }
+  return check;
+}
 
 int main() {
   char one;
@@ -37,7 +51,7 @@ int main() {
     printf(
         "1. 피부타입 진단하기\n2. 나의 화장품 서랍\n3. 오늘의 피부 일기\n4. "
         "올리브영 바로가기\n5. 여드름 관련 정보 모음zip\n");
-    printf("6. 프로그램 종료\n");
+    printf("6. 설정\n.7. 프로그램 종료\n");
     /*printf("오늘(날짜~)은 약 먹는 날이 %s\n", med;*/
     printf("--------------------------\n");
     scanf_s("%d", &choice);
@@ -47,27 +61,43 @@ int main() {
       printf("피부타입 진단을 시작할게요 :)\n");
       printf("피부타입은 크게 지성, 복합성(중성), 건성으로 나누어져요!\n");
       printf("다음 보기들을 보고 자신의 피부와 가까운 것을 고르면 됩니다.\n");
-      printf(
-          "1. 세수하고 나와서 아무것도 안바른 상태로 시간이 지나도 건조하지가 "
-          "않다. 2. 세수하고 아무것도 안바른다면 얼굴이 건조하고 당기는 "
-          "편이다.\n");
-      scanf_s("%d", &type[0]);
-      printf(
-          "1. 오후가 되면 얼굴에 기름이 뜨고 반짝거린다. 2. 오후가 되어도 "
-          "아침과 피부상태가 똑거나 기름이 거의 없다.\n");
-      scanf_s("%d", &type[1]);
-      printf(
-          "1. 가을겨울에도 피부의 건조함을 느끼지 못한다. 2. 가을겨울은 피부가 "
-          "다른 계절보다 건조해진다.\n");
-      scanf_s("%d", &type[2]);
-      printf(
-          "1. 기름종이를 사용했을 때 기름이 잔뜩 흡수된다. 2. 기름종이를 "
-          "썼는데 "
-          "기름종이가 뽀송하다. (기름종이가 없다면 깨끗한 손으로 얼굴을 "
-          "만져보고 묻은 양을 확인해보세요)\n");
-      scanf_s("%d", &type[3]);
-      printf("1. 여드름이 많이 나있다. 2. 여드름이 없는 편이다.\n");
-      scanf_s("%d", &type[4]);
+      do {
+        printf(
+            "1. 세수하고 나와서 아무것도 안바른 상태로 시간이 지나도 "
+            "건조하지가 "
+            "않다. 2. 세수하고 아무것도 안바른다면 얼굴이 건조하고 당기는 "
+            "편이다.\n");
+        scanf_s("%d", &type[0]);
+        check = errordetect(*(type + 0), 2);
+      } while (check);
+      do {
+        printf(
+            "1. 오후가 되면 얼굴에 기름이 뜨고 반짝거린다. 2. 오후가 되어도 "
+            "아침과 피부상태가 똑같거나 기름이 거의 없다.\n");
+        scanf_s("%d", &type[1]);
+        check = errordetect(*(type + 1), 2);
+      } while (check);
+      do {
+        printf(
+            "1. 가을겨울에도 피부의 건조함을 느끼지 못한다. 2. 가을겨울은 "
+            "피부가 "
+            "다른 계절보다 건조해진다.\n");
+        scanf_s("%d", &type[2]);
+        check = errordetect(*(type + 2), 2);
+      } while (check);
+      do {
+        printf(
+            "1. 기름종이를 사용했을 때 기름이 잔뜩 흡수된다. 2. 기름종이를 "
+            "썼는데 기름종이가 뽀송하다. (기름종이가 없다면 깨끗한 손으로 "
+            "얼굴을 만져보고 묻은 양을 확인해보세요)\n");
+        scanf_s("%d", &type[3]);
+        check = errordetect(*(type + 3), 2);
+      } while (check);
+      do {
+        printf("1. 여드름이 많이 나있다. 2. 여드름이 없는 편이다.\n");
+        scanf_s("%d", &type[4]);
+        check = errordetect(*(type + 4), 2);
+      } while (check);
 
       for (int i = 0; i < 5; i++) {
         sum += *(type + i);
@@ -77,14 +107,18 @@ int main() {
         printf(
             "지성은 일반적으로 유분(기름)이 많은 피부입니다. 꾸덕하거나 잘 "
             "흡수되지 않는 무거운 기초화장품을 사용하면 여드름이 더 심해지니 "
-            "주의하세요!\n");
+            "주의하세요! 또한 기름종이 사용을 추천합니다.\n");
       } else if (sum <= 8) {
         printf("피부 타입 진단 결과는 중성(복합성)입니다!\n");
-        printf("~~\n");
+        printf(
+            "중성은 기름이 너무 많이 나오지도, 너무 적지도 않은 피부입니다.\n");
       } else {
         printf("피부 타입 진단 결과는 건성입니다!\n");
-        printf("건성은 ~\n");
-      }  // 함수로 만들기
+        printf(
+            "건성은 얼굴에 기름이 별로 나오지 않는 피부입니다. 그렇기에 보습을 "
+            "잘 해주는 것이 중요합니다. 피부가 건조해지지 않도록 꼭 "
+            "기초화장품을 발라주세요.\n");
+      }
 
     } else if (choice == 2) {
       int num = 0;
@@ -109,6 +143,13 @@ int main() {
       }
     } else if (choice == 3) {
       int num = 0;
+      char* fileName = "./hwa_diary.txt";  // 일기 저장 txt
+      FILE* file;
+      fopen_s(&file, fileName, "a+");
+      if (file == NULL) {
+        printf("실패\n");
+        return 1;
+      }
       printf(
           "1. 피부 일기 쓰기\n2. 피부 일기 수정\n3. 피부 "
           "일기 보기\n");
@@ -120,12 +161,29 @@ int main() {
         scanf_s("%d", &month);
         printf("일을 입력하세요.(1~31)\n");
         scanf_s("%d", &day);
-        // 파일입출력 ~
+        printf("%d월 %d일", month, day);
+        fprintf(file, "%d월 %d일", month, day);
+        printf("일기 입력(작성이 완료되면 나가기를 입력하세요): ");
+        while (1) {
+          scanf_s("%[^\n]", content, (int)sizeof(content));
+          getchar();
+
+          if (strcmp(content, "나가기") == 0) {  // 문자열 비교 같으면 0반환하는거임
+            printf("일기가 저장되었습니다.\n");
+            break;
+          }
+          fputs(content, file);
+          fputs("\n", file);
+        }
+        fclose(file);
+
       } else if (num == 2) {
         int num = 0;
         printf("1. 날짜수정\n2. 일기내용수정\n");
         scanf_s("%d", &num);
-
+        if (num == 1) {
+        } else if (num == 2) {
+        }
       } else if (num == 3) {
       }
     } else if (choice == 4) {
@@ -169,10 +227,11 @@ int main() {
                                                                        // ㄱ
       }
     } else if (choice == 6) {
+    } else if (choice == 7) {
       printf("프로그램을 종료합니다.\n");
       return;
     } else {
-      printf("잘못된 입력입니다. 1~6번 사이의 번호를 입력해주세요\n");
+      printf("잘못된 입력입니다. 1~7번 사이의 번호를 입력해주세요\n");
     }
   }
 }
@@ -193,6 +252,7 @@ void addHwaInfo(struct HwaInfo* hwa) {
   scanf_s("%f", &hwa->star);
 
   printf("한줄평을 입력하세요: ");
+
 }
 
 void displayHwaInfo(const struct HwaInfo* hwa, int count) {
@@ -200,7 +260,7 @@ void displayHwaInfo(const struct HwaInfo* hwa, int count) {
   for (int i = 0; i < count; i++) {
     printf("%d. %s | %s (%.1f/5)\n", i + 1, hwa[i].brandname, hwa[i].name,
            hwa[i].star);
-    // printf("%s"); 한줄평 출력
+    printf("%s\n", hwa[i].write);  // 한줄평 출력
   }
 }
 // 할일 동적메모리 free
